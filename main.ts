ab4cbdcecd74bb256ceae73e9eef0874f6636adc
johnsoe2 <34690013+johnsoe2@users.noreply.github.com>
2020-07-21 08:56:25 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,438 @@
+namespace SpriteKind {
+    export const Hat = SpriteKind.create()
+    export const Label = SpriteKind.create()
+}
+namespace StatusBarKind {
+    export const Health = StatusBarKind.create()
+    export const Energy = StatusBarKind.create()
+    export const Magic = StatusBarKind.create()
+    export const EnemyHealth = StatusBarKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+c c c c c c c c c c c c c c c c 
+b c b c b c b c b c b c b c b c 
+b c b c b c b c b c b c b c b c 
+b c b c b c b c b c b c b c b c 
+b c b c b c b c b c b c b c b c 
+b c b c b c b c b c b c b c b c 
+b c b c b c b c b c b c b c b c 
+b c b c b c b c b c b c b c b c 
+b c b c b c b c b c b c b c b c 
+b c b c b c b c b c b c b c b c 
+b c b c b c b c b c b c b c b c 
+c c c c c c c c c c c c c c c c 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. c c c c c c c c c c c c c c c 
+. c b c b c b c b c b c b c b c 
+. c b c b c b c b c b c b c b c 
+. c b c b c b c b c b c b c b c 
+. c b c b c b c b c b c b c b c 
+. c b c b c b c b c b c b c b c 
+. c b c b c b c b c b c b c b c 
+. c b c b c b c b c b c b c b c 
+. c b c b c b c b c b c b c b c 
+. c b c b c b c b c b c b c b c 
+. c b c b c b c b c b c b c b c 
+. c c c c c c c c c c c c c c c 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+c c c c 3 3 3 3 3 c c c c c . . 
+c b c 3 b c b c b 3 b c b c . . 
+c b 3 c b c b c b c b c b c . . 
+c 3 c c 3 3 3 3 3 3 b c b c . . 
+3 b c c 3 c b c b c 3 c b c . . 
+3 b c c b 3 b c b c 3 c b c . . 
+3 b c c b 3 b c 3 3 b 3 b c . . 
+c 3 c c b c 3 3 b c b 3 b c . . 
+c 3 c c b c b c b c b 3 b c . . 
+c b 3 c b c b c b c 3 c b c . . 
+c b 3 c b c b 3 3 3 b c b c . . 
+c c c 3 3 3 3 c c c c c c c . . 
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c . . 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+c c c c c c c c c c c c c c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c c c c c c c c c c c c c c . . 
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c . . 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c . . 
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c . . 
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c . . 
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c . . 
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c . . 
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c . . 
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c . . 
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c . . 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+. . c c c c c c c c c c c c . . 
+. . c b b b b b b b b b b c . . 
+c c c c c c c c c c c c c c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c b c c b c b c b c b c b c . . 
+c c c c c c c c c c c c c c . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+. . c c c c c c c c c c c c . . 
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c c c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c c c c c c c c c c c c c 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile8 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . c c c c c c c c c c c c c c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c b c b c b c b c b c b c 
+. . c c c c c c c c c c c c c c 
+. . c b b b b b b b b b b c . . 
+. . c c c c c c c c c c c c . . 
+`
+}
+function positionSelection () {
+    if (shopIsOpen) {
+        selectionBackground.y = shopTop - 1 + shopSelection * 10
+    }
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (shopIsOpen) {
+        shopSelection = (shopSelection + 1) % products.length
+        positionSelection()
+    }
+})
+function closeShop () {
+    for (let value of sprites.allOfKind(SpriteKind.Text)) {
+        value.destroy()
+    }
+    shopIsOpen = false
+    showHideHats(true)
+    tiles.setTilemap(tiles.createTilemap(
+            hex`0b00080000000000000000000000000000000000000000000000000201010101010104000000000000000000000500000008010101010101060000000500000000000000000000070101010101010101030000000000000000000000`,
+            img`
+2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . 2 2 
+2 2 2 2 2 2 2 2 . 2 2 
+2 . . . . . . . . 2 2 
+2 . 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8],
+            TileScale.Sixteen
+        ))
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (shopIsOpen) {
+        closeShop()
+    } else {
+        openShop()
+    }
+})
+/**
+ * Paper hat fold-a-tron
+ * 
+ * Haberdasher-9000
+ * 
+ * Ribbon twirler X
+ */
+function openShop () {
+    tiles.setTilemap(tiles.createTilemap(
+            hex`0b00080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
+            img`
+. . . . . . . . . . . 
+. . . . . . . . . . . 
+. . . . . . . . . . . 
+. . . . . . . . . . . 
+. . . . . . . . . . . 
+. . . . . . . . . . . 
+. . . . . . . . . . . 
+. . . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8],
+            TileScale.Sixteen
+        ))
+    showHideHats(false)
+    shopIsOpen = true
+    shopSelection = 0
+    shopTop = 20
+    for (let index = 0; index <= products.length - 1; index++) {
+        productLabel = textsprite.create(products[index], 0, 15)
+        productLabel.setMaxFontHeight(8)
+        productLabel.left = 10
+        productLabel.y = shopTop + index * 10
+        productLabel.z = 5
+        productPrice = textsprite.create("$" + productPrices[index], 0, 15)
+        productPrice.setMaxFontHeight(8)
+        productPrice.right = 150
+        productPrice.y = shopTop + index * 10
+        productPrice.z = 5
+    }
+    selectionBackground = sprites.create(image.create(144, 12), SpriteKind.Text)
+    selectionBackground.image.fill(5)
+    positionSelection()
+}
+function addPoint (pt: number, source: number) {
+    if (!(shopIsOpen)) {
+        info.changeScoreBy(pt)
+        statusbar.value += pt
+        if (statusbar.value == 100) {
+            buildHat(source)
+        }
+    }
+}
+function buildHat (creator: number) {
+    statusbar.value = 0
+    newHat = sprites.create(list[creator], SpriteKind.Hat)
+    tiles.placeOnRandomTile(newHat, myTiles.tile2)
+    scene.followPath(newHat, hatPath, 30)
+    newHat.setFlag(SpriteFlag.AutoDestroy, true)
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (shopIsOpen) {
+        buyProduct()
+    } else {
+        addPoint(1, 0)
+    }
+})
+function showHideHats (show: boolean) {
+    showKinds = [SpriteKind.StatusBar, SpriteKind.Hat]
+    for (let value of showKinds) {
+        sprite_list = sprites.allOfKind(value)
+        for (let value of sprite_list) {
+            value.setFlag(SpriteFlag.Invisible, !(show))
+        }
+    }
+}
+function createLabels () {
+    for (let value of sprites.allOfKind(SpriteKind.Label)) {
+        value.destroy()
+    }
+    for (let index = 0; index <= list.length - 2; index++) {
+        hatLabel = textsprite.create("" + productQuantities[index])
+        hatLabel.setIcon(list[index + 1])
+        hatLabel.setMaxFontHeight(8)
+        hatLabel.y = 8
+        hatLabel.left = index * 24 + 2
+        hatLabel.setKind(SpriteKind.Label)
+    }
+}
+function buyProduct () {
+    if (info.score() >= productPrices[shopSelection]) {
+        info.changeScoreBy(0 - productPrices[shopSelection])
+        productQuantities[shopSelection] = productQuantities[shopSelection] + 1
+        createLabels()
+    }
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (shopIsOpen) {
+        shopSelection = (shopSelection + (products.length - 1)) % products.length
+        positionSelection()
+    }
+})
+let hatLabel: TextSprite = null
+let sprite_list: Sprite[] = []
+let showKinds: number[] = []
+let newHat: Sprite = null
+let productPrice: TextSprite = null
+let productLabel: TextSprite = null
+let shopSelection = 0
+let shopTop = 0
+let selectionBackground: Sprite = null
+let shopIsOpen = false
+let hatPath: tiles.Location[] = []
+let statusbar: StatusBarSprite = null
+let list: Image[] = []
+let productQuantities: number[] = []
+let productPrices: number[] = []
+let products: string[] = []
+products = ["FOLD-A-TRON", "HABERDASHER-9000", "RIBBON-TWIRLER X"]
+productPrices = [50, 100, 200]
+productQuantities = [0, 0, 0]
+list = [img`
+. . . . . . . . . . . . . 
+. . 6 6 6 6 6 6 6 . . . . 
+. 6 6 6 6 6 6 6 6 6 . . . 
+6 6 6 6 6 6 6 6 1 1 1 . . 
+6 6 6 6 6 6 6 6 1 1 1 . . 
+6 6 6 6 6 6 6 6 1 1 1 . . 
+b 6 6 6 6 6 6 6 1 1 1 . . 
+6 6 6 6 6 6 6 6 6 6 6 6 6 
+`, img`
+. . . . . . b . . . . . . 
+. . . . . b 1 b . . . . . 
+. . . . b 1 1 1 b . . . . 
+. . . b 1 1 1 1 1 b . . . 
+b b b b b b b b b b b b b 
+b 1 1 1 1 1 1 1 1 1 1 1 b 
+. b 1 1 1 1 1 1 1 1 1 b . 
+. . b b b b b b b b b . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . c c c c c c . . . . . 
+. . . . . c f f f f c . . . . . 
+. . . . . c f f f f c . . . . . 
+. . . . . c f f f f c . . . . . 
+. . . . . c f f f f c . . . . . 
+. . . . . c f f f f c . . . . . 
+. . . . . c f f f f c . . . . . 
+. . . c c c 1 5 5 1 c c c . . . 
+. . c f f c f f f f c f f c . . 
+. . c f f f f f f f f f f c . . 
+. . . c c c c c c c c c c . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . 5 5 5 5 5 5 5 . . . . . 
+. . . 5 5 5 5 5 5 5 5 5 . . . . 
+. . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+. . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+. . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+. 5 2 2 2 2 2 2 2 2 2 2 2 5 . . 
+5 5 5 5 5 5 5 5 5 5 5 5 2 5 5 . 
+. 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`]
+scene.setBackgroundColor(13)
+tiles.setTilemap(tiles.createTilemap(
+            hex`0b00080000000000000000000000000000000000000000000000000201010101010104000000000000000000000500000008010101010101060000000500000000000000000000070101010101010101030000000000000000000000`,
+            img`
+2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . 2 2 
+2 2 2 2 2 2 2 2 . 2 2 
+2 . . . . . . . . 2 2 
+2 . 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8],
+            TileScale.Sixteen
+        ))
+statusbar = statusbars.create(140, 8, StatusBarKind.Magic)
+statusbar.top = 15
+statusbar.setStatusBarFlag(StatusBarFlag.SmoothTransition, false)
+statusbar.setBarBorder(1, 11)
+statusbar.value = 0
+let lastHatX = 10
+let lastHatY = 30
+info.setScore(0)
+createLabels()
+hatPath = scene.aStar(tiles.getTileLocation(1, 2), tiles.getTileLocation(10, 6))
+game.showLongText("Welcome to the Hat Factory. Press A for HAT. Press B for BUY ROBOT.", DialogLayout.Bottom)
+game.onUpdateInterval(500, function () {
+    for (let index = 0; index < productQuantities[2]; index++) {
+        addPoint(1, 3)
+    }
+})
+game.onUpdateInterval(5000, function () {
+    for (let index = 0; index < productQuantities[0]; index++) {
+        addPoint(1, 1)
+    }
+})
+game.onUpdateInterval(1000, function () {
+    for (let index = 0; index < productQuantities[1]; index++) {
+        addPoint(1, 2)
+    }
+})
